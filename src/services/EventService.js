41572ecee37f804fc749f589f7454bbfd75ee146
kmalmaulana <akmalmaulana701@gmail.com>
2021-08-14 16:47:27 +0700

    add express

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
@@ -7,13 +7,23 @@ const apiClient = axios.create({
     Accept: 'application/json',
     'Content-Type': 'application/json'
   }
-})
+});
+
+const express = require('express');
+const app = express();
+
+// Serve all the files in '/dist' directory
+app.use(express.static('dist'));
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
 
 export default {
   getEvents() {
-    return apiClient.get('/events')
+    return apiClient.get('/events');
   },
   getEvent(id) {
-    return apiClient.get('/events/' + id)
+    return apiClient.get('/events/' + id);
   }
-}
+};
