e923c8cddc5d5c4781541e5efde032373bd254f9
Akmal Maulana <akmalmaulana701@gmail.com>
2021-08-14 16:52:10 +0700

    remove express

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -9,17 +9,6 @@ const apiClient = axios.create({
   }
 })
 
-const express = require('express');
-const app = express();
-
-// Serve all the files in '/dist' directory
-app.use(express.static('dist'));
-
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
 export default {
   getEvents() {
     return apiClient.get('/events')
